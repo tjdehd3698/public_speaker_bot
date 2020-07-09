1af363eefdb5ab7b8a2eea8483b58296bd232cc9
tjdehd3698 <khdkhd12@gmail.com>
2020-07-09 16:45:03 +0900

    메뉴 정리

--- public-speaker-bot/bot.js
+++ public-speaker-bot/bot.js
@@ -10,8 +10,12 @@ class MyBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             const text=context.activity.text;
-            if (text == "목적 알려주세요") {
+            if (text == "목적 알려주세요"||text=="목적알려주세요") {
                 await context.sendActivity(`현대인들의 저조한 독서율을 올리기 위해 도서 추천을 해줍니다.`);
+                await next();
+            }
+            else if (text == "도서추천"||text=="도서 추천") {
+                await this.sendSuggestedActions(context);
                 await next();
             }
             else {
@@ -21,15 +25,15 @@ class MyBot extends ActivityHandler {
             }
         });
         this.onConversationUpdate(async (context, next) => {
-            await context.sendActivity('Welcome');
-            await this.sendSuggestedActions(context);
-            // By calling next() you ensure that the next BotHandler is run.
+            await context.sendActivity('어서오세요');
+            await context.sendActivity('"목적 알려주세요" 또는 "도서추천"을 입력해주세요');
+           // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
     }
     async sendSuggestedActions(turnContext) {
-        var reply = MessageFactory.suggestedActions(['베스트셀러', '화제의 신작'], '무엇을 보고싶나요?');
-        await turnContext.sendActivity(reply);
+            var reply = MessageFactory.suggestedActions(['베스트셀러', '화제의 신작', '나가기'], '무엇을 보고싶나요?');
+            await turnContext.sendActivity(reply);
     }
 }
 
