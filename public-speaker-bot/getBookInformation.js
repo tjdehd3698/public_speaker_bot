66bacb676f23916d8cf3279a788f6a53ce5c4747
Noh Hyeon Jin <hjforj@naver.com>
2020-07-15 16:09:50 +0900

    Update getBookInformation.js

--- public-speaker-bot/getBookInformation.js
+++ public-speaker-bot/getBookInformation.js
@@ -4,17 +4,7 @@ const cheerio = require("cheerio");
 var bookName = '수제비정보처리기사';    //책제목 하드코딩 -> 나중에 수정
 var getBarcodeUrl = 'https://search.kyobobook.co.kr/web/search?vPstrKeyWord=' + encodeURI(bookName) + '&orderClick=LET';
 
-//책 바코드 html 가져오기
-async function getBookBarcodeHTML() {
-    try {
-        return await axios.get(getBarcodeUrl);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-//책 정보 html 가져오기
-async function getBookInformationHTML(url) {
+async function getHTML(url) {
     try {
         return await axios.get(url);
     } catch (error) {
@@ -22,15 +12,33 @@ async function getBookInformationHTML(url) {
     }
 }
 
-getBookBarcodeHTML().then(html => { //바코드 가져오기
+getHTML(getBarcodeUrl).then(html => {
     const $ = cheerio.load(html.data);
     const bodyList = $("tbody#search_list").children("input");
-    var bookBarcode = $(bodyList[0]).val();
-    return bookBarcode;
-}).then(bookBarcode => {    //가져온 바코드로 책 정보 검색
-    var branch = '05';    //지점코드 하드코딩 -> 나중에 수정
-    var getLocationUrl = 'http://mkiosk.kyobobook.co.kr/kiosk/product/bookInfoInk.ink?site=' + branch + '&ejkGb=KOR&barcode=' + bookBarcode + '&map=Y&orderClick=JFH';
-    return getBookInformationHTML(getLocationUrl);
+    var bookBarcode = $(bodyList[0]).val(); //책 바코드 가져오기
+    console.log('책 바코드 : '+bookBarcode);
+    var getBranchCodeUrl = 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=' + bookBarcode + '&orderClick=LET&Kc=';
+    return getHTML(getBranchCodeUrl);
+}).then(html => {
+    // 지점코드 가져오는 부분
+    /*
+    const $ = cheerio.load(html.data);
+    const table = $('#storeStockTable > table');
+    const tr_list = $(table).children("tbody").children("tr");
+    for (var row = 0; row < tr_list.length; row++) {
+        var cells = tr_list.eq(row).children();
+        var cols = [];
+        for (var col = 0; col < cells.length; col++) {
+            var value = cells.eq(col).text();
+            cols.push(value);
+            console.log(value);
+        }
+    }
+    */
+    return branchCode;
+}).then(branchCode => {
+    var getLocationUrl = 'http://mkiosk.kyobobook.co.kr/kiosk/product/bookInfoInk.ink?site=' + branchCode + '&ejkGb=KOR&barcode=' + bookBarcode + '&map=Y&orderClick=JFH';
+    return getHTML(getLocationUrl);
 }).then(html => {   //책 정보(재고, 위치, 위치이미지) 가져오기
     const $ = cheerio.load(html.data);
     const bookStock = $("div.p_stock").children('strong').text();   //재고 가져오기
