e8af57154d7018236e8c212ae8c64f8024763ded
Noh Hyeon Jin <hjforj@naver.com>
2020-07-16 14:09:08 +0900

    코드 정리

--- public-speaker-bot/getBook.js
+++ public-speaker-bot/getBook.js
@@ -1,23 +1,18 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function getBestSellerHTML() {
+var getBestSellerUrl = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%B2%A0%EC%8A%A4%ED%8A%B8%EC%85%80%EB%9F%AC";
+var getNewBooksUrl = "https://search.shopping.naver.com/search/all?query=%EC%8B%A0%EC%9E%91%EB%8F%84%EC%84%9C&frm=NVSCPRO&bt=0";
+
+async function getHTML(url) {
   try {
-    return await axios.get("https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%B2%A0%EC%8A%A4%ED%8A%B8%EC%85%80%EB%9F%AC");
+    return await axios.get(url);
   } catch (error) {
     console.error(error);
   }
 }
 
-async function getNewBooksHTML() {
-  try {
-      return await axios.get("https://search.shopping.naver.com/search/all?query=%EC%8B%A0%EC%9E%91%EB%8F%84%EC%84%9C&frm=NVSCPRO&bt=0");
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getBestSellerHTML().then(html => {
+getHTML(getBestSellerUrl).then(html => {
   let liList = [];
   const $ = cheerio.load(html.data);
   const bodyList = $("div.cs_bestseller").children("ol.thumb_list").children("li");
@@ -31,7 +26,7 @@ getBestSellerHTML().then(html => {
   return liList;
 }).then(res => module.exports.bestSeller = res)
 
-getNewBooksHTML().then(html => {
+getHTML(getNewBooksUrl).then(html => {
   let liList = [];
   const $ = cheerio.load(html.data);
   const bodyList = $(".basicList_title__3P9Q7");
