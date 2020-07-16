cf5d8e9e691ce22c08e391771837cf7c7802830e
Noh Hyeon Jin <hjforj@naver.com>
2020-07-16 21:32:25 +0900

    Update predict.js

--- public-speaker-bot/predict.js
+++ public-speaker-bot/predict.js
@@ -1,48 +1,21 @@
 //
 // This quickstart shows how to predict the intent of an utterance by using the LUIS REST APIs.
 //
+require("dotenv").config();
 var requestPromise = require('request-promise');
 var queryString = require('querystring');
 
-const fs = require('fs');
-//const Json = require('query-json.json');
-
 // Analyze a string utterance.
-getPrediction = async () => {
-    //const fs = require('fs')
-    //////////
-    // Values to modify.
-    const utf8 = require('utf8');
-    const iconv = require("iconv-lite");
-    // YOUR-APP-ID: The App ID GUID found on the www.luis.ai Application Settings page.
-    const LUIS_appId = "93a58891-bd1a-48fd-b48d-88dd89ad84ae";
-
-    // YOUR-PREDICTION-KEY: Your LUIS authoring key, 32 character value.
-    const LUIS_predictionKey = "751041c1212641bfa0fc5ef2eb83202d";
-
-    // YOUR-PREDICTION-ENDPOINT: Replace this with your authoring key endpoint.
-    // For example, "https://westus.api.cognitive.microsoft.com/"
-    const LUIS_endpoint = "https://mybook-luis.cognitiveservices.azure.com/";
-    //const utterance = " "
-
-    var utterance = "포항공대점에 말센스 어디 있어?";
-
-    const dataBuffer = fs.readFileSync('query-json.json')
-    const dataJson = dataBuffer.toString()
-
-    const data = JSON.parse(dataJson)
-    console.log('쿼리: ' + data.query)
-    console.log('적합성: ' + data.prediction.intents.위치.score)
-    console.log('책이름: ' + data.prediction.entities.책[1].이름);
-    console.log('지점: ' + data.prediction.entities.책[0].지점);
-    //const utterance = data.query;
-    
+module.exports.getPrediction = async (str) => {
+    const LUIS_appId = process.env.LUISAPPID;
+    const LUIS_predictionKey = process.env.LUISPREDICTIONKEY;
+    const LUIS_endpoint = process.env.LUISENDPOINT;
 
     // Create query string
     const queryParams = {
         "show-all-intents": true,
         "verbose": true,
-        "query": utterance,
+        "query": str,
         "subscription-key": LUIS_predictionKey
     }
 
@@ -50,14 +23,17 @@ getPrediction = async () => {
     const URI = `${LUIS_endpoint}luis/prediction/v3.0/apps/${LUIS_appId}/slots/production/predict?${queryString.stringify(queryParams)}`
 
     // Send the REST call.
-    const response = await requestPromise(URI);
+    const response = JSON.parse(await requestPromise(URI));
+    
+    const score = response.prediction.intents.위치.score;
+    const userBookName = response.prediction.entities.책[1].이름[0];
+    const userBranch = response.prediction.entities.책[0].지점[0];
 
-    const bookJson = JSON.stringify(response);
+    var predictObj = {
+        'score' : score,
+        'bookName' : userBookName,
+        'branch' : userBranch
+    };
 
-
-    console.log(response);
+    return predictObj;
 }
-
-// Pass an utterance to the sample LUIS app
-getPrediction().then(() => console.log("done")).catch((err) => console.log(err));
-//exports.getPrediction = this.getPrediction;
